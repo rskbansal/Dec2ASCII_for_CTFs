7f98e2c7a172ede7e25e1c16da44da0e3c08db9b
Rhythm Bansal <rskbansal@outlook.com>
2022-12-05 16:51:37 +0530

    enabled check function

--- dec2ascii/__main__.py
+++ dec2ascii/__main__.py
@@ -8,8 +8,8 @@ if(len(sys.argv) == 1):
 else:
   if(sys.argv[1].isnumeric()):
     decoded = dec2ascii.decode(sys.argv[1])
-    print(f"{decoded}")
-    # print(f"{dec2ascii.check(decoded)}")
+    print(f"{decoded}\n")
+    print(f"{dec2ascii.check(decoded)}")
     sys.exit(0)
 
   else:
